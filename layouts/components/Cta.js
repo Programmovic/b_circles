7ae810090e4f749c939af4d274b124023d49c5d8
Adam <adamsoliman800@gmail.com>
2023-12-22 00:48:47 +0300

    update Cta.js and base.scss

--- layouts/components/Cta.js
+++ layouts/components/Cta.js
@@ -9,29 +9,14 @@ function Cta({ cta }) {
 
   return (
     <section className="section px-4 dark:bg-[#231f20]">
-      <div className="container pt-[5em]">
+      <div className="container">
         <div className="row mx-auto items-center justify-center">
-          
           <div className="text-center md:mt-0 md:col-12 lg:col-12">
+            <div className="seven">
+              <h1 className="section_title">Follow b-circles</h1>
+            </div>
             {cta?.button.enable && (
               <>
-                {/* <div className="drawer">
-                  <div className="box">
-                    <Link href={twitter} className="ball ball-1 twitter"><FaTwitter /></Link>
-                    <Link href={facebook} className="ball ball-2 github"><FaFacebook /></Link>
-                    <Link href="https://www.nourabusoud.com" className="ball ball-3 website"><FaInstagram /></Link>
-                    <Link
-                      className="ball ball-4"
-                      href={cta?.button.link}
-                      rel={cta?.button.rel}
-                      title={cta?.button.label}
-                    >
-                      <FaPhone />
-                    </Link>
-                  </div>
-                  <div className="cover"><span /></div>
-                </div> */}
-                {/* Additional HTML structure */}
                 <div className="container">
                   <ul className="menu">
                     <li><Link href="https://www.facebook.com/bcirclesagency" className="facebook"><FaFacebook /></Link></li>
@@ -48,3 +33,4 @@ function Cta({ cta }) {
 }
 
 export default Cta;
+
