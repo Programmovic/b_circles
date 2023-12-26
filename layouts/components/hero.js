c9791492ed9e634fbedb4ca869bb4b0ce08c9818
Adam <adamsoliman800@gmail.com>
2023-12-27 00:08:31 +0300

    update hero.js

--- layouts/components/hero.js
+++ layouts/components/hero.js
@@ -44,7 +44,7 @@ function Hero({ banner }) {
   return (
 
     <section className='relative hero_section overflow-hidden'>
-      
+
       <ul className='circles'>
         <li className="circle" />
         <li className="circle" />
@@ -65,77 +65,91 @@ function Hero({ banner }) {
         </div>
         <div className="hero__overlay bg-[#ffffff54] dark:bg-[#0000008f]"></div>
         <div className="hero__cnt">
-        <div className="container">
-          <motion.div
-            className="lg:col-12 hero-img"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6 }}
-          >
-            <motion.img
-              className="mx-auto mb-5 animated-logo pointer-events-none select-none"
-              src={banner?.image}
-              width={200}
-              height={200}
-              alt="banner image"
-              priority
-            />
-          </motion.div>
-          <motion.div
-            className="lg:col-12 text-center"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6 }}
-          >
-            <div className="font-third font-bold text-white mb-2">
-              <h1 className="m-0 text-[20px] uppercase text-dark dark:text-white font-third">B-Circles Agency</h1>
-              <div className='py-4 flex justify-between __actions'>
-                <Link
-                  className="inline-block rounded-full border-2  border-neutral-900 px-[30px] pt-[14px] pb-[12px] text-sm uppercase leading-normal text-neutral-900 font-bold transition duration-150 ease-in-out hover:border-neutral-100 bg-[#4552a4ab] hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:border-neutral-100 dark:text-neutral-100"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                  title="get free consultation"
-                  href={`/free-consultation`}
-                  role="button"
-                >
-                  get a free consultation
-                </Link>
-                <Link
-                  className="inline-block rounded-full px-12 pt-[14px] pb-[12px] text-sm uppercase leading-normal text-neutral-900 font-bold transition duration-150 ease-in-out bg-[#e0692396] hover:bg-neutral-500 hover:bg-opacity-50 hover:text-neutral-200 focus:text-neutral-200 focus:outline-none focus:ring-0 active:text-neutral-300 dark:text-neutral-100"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                  href={`/contact-us`}
-                  title="Contact"
-                  role="button"
-                >
-                  Contact
-                </Link>
+          <div className="container">
+            <motion.div
+              className="lg:col-12 hero-img"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+            >
+              <div className='relative'>
+              <motion.img
+                className="mx-auto mb-5 pointer-events-none select-none"
+                src={banner?.image}
+                width={200}
+                height={200}
+                alt="banner image"
+                priority
+              />
+              <img
+  className="mx-auto mb-5 christmas-hat absolute"
+  src="/images/hat.png"
+  alt="Christmas hat"
+  style={{
+    transform: 'translate(-50%, -50%) scaleX(-1) rotate(18deg)',
+    top: 'calc(50% - 97px)', // Adjust the top position as needed
+    left: 'calc(50% + 89px)', // Adjust the left position as needed
+    width: '100px',
+  }}
+/>
 
               </div>
-            </div>
+            </motion.div>
+            <motion.div
+              className="lg:col-12 text-center"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6 }}
+            >
+              <div className="font-third font-bold text-white mb-2">
+                <h1 className="m-0 text-[20px] uppercase text-dark dark:text-white font-third">B-Circles Agency</h1>
+                <div className='py-4 flex justify-between __actions'>
+                  <Link
+                    className="inline-block rounded-full border-2  border-neutral-900 px-[30px] pt-[14px] pb-[12px] text-sm uppercase leading-normal text-neutral-900 font-bold transition duration-150 ease-in-out hover:border-neutral-100 bg-[#4552a4ab] hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:border-neutral-100 dark:text-neutral-100"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                    title="get free consultation"
+                    href={`/free-consultation`}
+                    role="button"
+                  >
+                    get a free consultation
+                  </Link>
+                  <Link
+                    className="inline-block rounded-full px-12 pt-[14px] pb-[12px] text-sm uppercase leading-normal text-neutral-900 font-bold transition duration-150 ease-in-out bg-[#e0692396] hover:bg-neutral-500 hover:bg-opacity-50 hover:text-neutral-200 focus:text-neutral-200 focus:outline-none focus:ring-0 active:text-neutral-300 dark:text-neutral-100"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                    href={`/contact-us`}
+                    title="Contact"
+                    role="button"
+                  >
+                    Contact
+                  </Link>
+
+                </div>
+              </div>
 
 
 
-            <div className='flex justify-center'>
-              <h1 className="text-[1em] pt-2 w-fit border-t border-solid dark:border-gray-500 border-black font-third font-bold text-dark dark:text-white m-0 uppercase __title">{banner?.title}</h1>
-            </div>
-            {banner?.button.enable && (
-              <motion.a
-                className="btn btn-primary mt-4"
-                href={banner?.button.link}
-                rel={banner?.button.rel}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.6, duration: 0.6 }}
-                title={banner?.button.label}
-              >
-                {banner?.button.label}
-              </motion.a>
-            )}
-          </motion.div>
+              <div className='flex justify-center'>
+                <h1 className="text-[1em] pt-2 w-fit border-t border-solid dark:border-gray-500 border-black font-third font-bold text-dark dark:text-white m-0 uppercase __title">{banner?.title}</h1>
+              </div>
+              {banner?.button.enable && (
+                <motion.a
+                  className="btn btn-primary mt-4"
+                  href={banner?.button.link}
+                  rel={banner?.button.rel}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.6, duration: 0.6 }}
+                  title={banner?.button.label}
+                >
+                  {banner?.button.label}
+                </motion.a>
+              )}
+            </motion.div>
           </div>
         </div>
-      
+
       </div>
 
 
