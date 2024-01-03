42dc233f4176624eae97fdc05d9acd71439a54c4
Adam <adamsoliman800@gmail.com>
2024-01-03 15:54:37 +0300

    update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,15 @@ const Document = () => {
     <Html lang="en">
       <Head>
         {/* favicon */}
+        <Script strategy="lazyOnload" id="google-analytics">
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MK8ZCMWN');
+  `}
+        </Script>
         <link
           rel="icon"
           type="image/png"
@@ -31,31 +40,25 @@ const Document = () => {
           media="(prefers-color-scheme: dark)"
           content="#000"
         />
-        <Script strategy="lazyOnload" id="google-analytics">
-  {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MK8ZCMWN');
-  `}
-</Script>
+
 
 
       </Head>
       <body className="dark:bg-[#1b1718] dark:text-white">
-        <Main />
-        {/* <TwSizeIndicator /> */}
-        <NextScript />
-        <Analytics />
-        {/* <noscript>
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-MK8ZCMWN"
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }} // Use an object for style
+            style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
-        </noscript> */}
+        </noscript>
+
+        <Main />
+        {/* <TwSizeIndicator /> */}
+        <NextScript />
+        <Analytics />
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js" async></script>
         <script src="https://cdn.jsdelivr.net/npm/vanta/dist/vanta.waves.min.js" async></script>
       </body>
